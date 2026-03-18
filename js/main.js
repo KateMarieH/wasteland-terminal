15e935d22c35c5d24407bb8ad4b9beffb5862d00
Lady Mystic <hill.m.katelyn@gmail.com>
2026-03-18 10:06:22 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,3 +17,28 @@ async function loadAnnouncement() {
 }
 
 loadAnnouncement();
+
+// Load Weather
+async function loadWeather() {
+  const panel = document.getElementById('weather');
+
+  try {
+    const res = await fetch('https://api.open-meteo.com/v1/forecast?latitude=37.6&longitude=-122.5&current_weather=true');
+    const data = await res.json();
+
+    const temp = Math.round(data.current_weather.temperature);
+    const desc = data.current_weather.weathercode;
+    const icon = getWeatherIcon(desc);
+
+    panel.innerHTML = `
+      <div class="weather-temp">${temp}°F</div>
+      <div class="weather-desc">${weatherDescription(desc)}</div>
+      <img class="weather-icon" src="${icon}" alt="">
+    `;
+  } catch (err) {
+    panel.textContent = "Weather unavailable";
+  }
+}
+
+loadWeather();
+
